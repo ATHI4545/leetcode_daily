8676d6e281d9fb2f1dc7594e68b8495e915c3a39
ATHI4545 <athikuti4@gmail.com>
2026-07-01 15:54:39 +0530

    Time: 31 ms (54.19%) | Memory: 48.4 MB (68.03%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : candyType) {
+            set.add(num);
+        }
+
+        return Math.min(set.size(), candyType.length / 2);
+    }
+}
