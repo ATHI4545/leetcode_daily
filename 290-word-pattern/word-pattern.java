3151b4612bdc1a5ef5e2b400962918da2d1e6301
ATHI4545 <athikuti4@gmail.com>
2026-07-13 10:30:46 +0530

    Time: 1 ms (84.16%) | Memory: 42.7 MB (55.27%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,19 @@
+class Solution {
+   public boolean wordPattern(String pattern, String str) {
+    String[] words=str.split(" ");
+
+    if(words.length!=pattern.length()){
+        return false;
+    }
+
+    Map index=new HashMap<>();
+
+    for(Integer i=0;i<words.length;++i){
+        if(index.put(words[i],i)!=index.put(pattern.charAt(i),i)){
+            return false;
+        }
+    }
+
+    return true;
+}
+}
