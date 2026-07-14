b2913f7f8871a8d082cda0f0fb37656e3b26e7a2
ATHI4545 <athikuti4@gmail.com>
2026-07-14 10:29:43 +0530

    Time: 1 ms (70.34%) | Memory: 43 MB (24.84%) - LeetSync

--- 504-base-7/base-7.java
+++ 504-base-7/base-7.java
@@ -1,5 +1,19 @@
-class Solution {
+ class Solution {
     public String convertToBase7(int num) {
-        return Integer.toString(num, 7);
+        if (num == 0) return "0";
+
+        boolean negative = num < 0;
+        num = Math.abs(num);
+        StringBuilder sb = new StringBuilder();
+
+        while (num > 0) {
+            int rem = num % 7;
+            sb.append(rem);   // append remainder
+            num /= 7;         // divide by 7
+        }
+
+        if (negative) sb.append('-');
+
+        return sb.reverse().toString();
     }
 }
